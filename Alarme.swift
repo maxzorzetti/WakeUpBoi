3cec7c945dddffa145c3c5037cf9cf37bfc3475d
Max Zorzetti <maxzorzetti@live.com>
2016-09-01 10:28:58 -0300

    Model de Alarme e Dia

---
+++ Alarme.swift
@@ -0,0 +1,17 @@
+//
+//  Alarme.swift
+//  WakeUpBoi
+//
+//  Created by Student on 9/1/16.
+//  Copyright © 2016 Dankness Edit. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Alarme: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
