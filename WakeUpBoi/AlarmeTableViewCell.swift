e4f40095daf45493b005aede5f5489ba636d1f80
Max Zorzetti <maxzorzetti@live.com>
2016-09-02 09:15:17 -0300

    Uns controllers e a classe de gif

---
+++ WakeUpBoi/AlarmeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmeTableViewCell.swift
+//  WakeUpBoi
+//
+//  Created by Student on 9/2/16.
+//  Copyright © 2016 Dankness Edit. All rights reserved.
+//
+
+import UIKit
+
+class AlarmeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
